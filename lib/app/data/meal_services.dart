39d8446bced049a73ad9906b021a3d9d500c224a
raiszidane95 <raiszidane95@gmail.com>
2023-03-04 07:28:25 +0700

    feat: create services API

---
+++ lib/app/data/meal_services.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+import '../modules/home/models/meals_models.dart';
+
+class MealService {
+  final _connect = Get.find<GetConnect>();
+
+  Future<Meals> getMeals() async {
+    final response = await _connect.get('1/filter.php?c=Seafood',
+        decoder: (data) => Meals.fromJson(data as Map<String, dynamic>));
+    if (!response.hasError) {
+      return response.body!;
+    } else {
+      throw Get.snackbar('Error', response.statusCode.toString());
+    }
+  }
+}
